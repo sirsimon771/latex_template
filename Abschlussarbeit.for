ffc0206e1c9431dd7b793dac98119b93462b9204
Simon Dischl <simon.dischl@yahoo.de>
2025-04-29 13:09:08 +0200

    initial commit

---
+++ Abschlussarbeit.for
@@ -0,0 +1,7 @@
+\contentsline {formel}{\numberline {1}{\ignorespaces Clarke-Transformation $I_\alpha $~{\blx@tocontentsinit {0}\cite [S.~309]{clarkeCircuitAnalysisAC1943}}}}{4}{formel.1}%
+\contentsline {formel}{\numberline {2}{\ignorespaces Clarke-Transformation $I_\beta $~{\blx@tocontentsinit {0}\cite [S.~309]{clarkeCircuitAnalysisAC1943}}}}{4}{formel.2}%
+\contentsline {formel}{\numberline {3}{\ignorespaces Clarke-Transformation in Matrixschreibweise}}{4}{formel.3}%
+\contentsline {formel}{\numberline {4}{\ignorespaces Park-Transformation~\blx@tocontentsinit {0}\cite {MathWorksPark}}}{4}{formel.4}%
+\contentsline {formel}{\numberline {5}{\ignorespaces Drehmomentgleichung aus magnetischem Fluss~{\blx@tocontentsinit {0}\cite [S.~507]{bolteElektrischeMaschinenGrundlagen2018}}}}{5}{formel.5}%
+\contentsline {formel}{\numberline {6}{\ignorespaces Zusammenhang magnetischer Fluss, Strom und Induktivität~{\blx@tocontentsinit {0}\cite [S.~43]{bolteElektrischeMaschinenGrundlagen2018}}}}{5}{formel.6}%
+\contentsline {formel}{\numberline {7}{\ignorespaces Drehmomentgleichung mit Induktivität}}{5}{formel.7}%
